93e223feba4202899779b2ee1450934701cc0cb3
Alison Davey <33370403+AlisonDavey@users.noreply.github.com>
2022-10-29 00:15:34 +0200

    Use environment variables

--- scripts/soil_moisture_data_to_tinybird.py
+++ scripts/soil_moisture_data_to_tinybird.py
@@ -3,6 +3,27 @@ import requests
 import pandas as pd
 
 import json
+import subprocess
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+UID = os.getenv('UID', None)
+PASSWORD = os.getenv('PASSWORD', None)
+TOKEN = os.getenv('TOKEN', None)
+
+def runcmd(cmd, *args, **kwargs):
+
+    process = subprocess.Popen(
+        cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        text = True,
+        shell = True
+    )
+    std_out, std_err = process.communicate()
+    pass
 
 def process_data(data, data_date, am_or_pm):
     
@@ -42,27 +63,32 @@ def process_data(data, data_date, am_or_pm):
         },
         data=data
     ) 
-    return r, data_date
+    return r, data_date, am_or_pm
 
 def main():
   for day in range(6,27):
 
-    fn="https://n5eil01u.ecs.nsidc.org/SMAP/SPL3SMP.008/2022.10."+("0"+str(day))[-2:]+"/SMAP_L3_SM_P_202210"+("0"+str(day))[-2:]+"_R18290_001.h5"
-    !wget --http-user={uid} --http-password={password}  --no-check-certificate --auth-no-challenge=on -r --reject "index.html*" -np -e robots=off $fn
-
+    url_start = "https://n5eil01u.ecs.nsidc.org/SMAP/SPL3SMP.008/2022.10."
+    url_end = "_R18290_001.h5"
+    fn = url_start+("0"+str(day))[-2:]+"/SMAP_L3_SM_P_202210"+("0"+str(day))[-2:]+url_end
+    
+    wget_call = 'wget --http-user='+UID+' --http-password='+PASSWORD+' --no-check-certificate --auth-no-challenge=on -r --reject "index.html*" -np -e robots=off '+fn
+    runcmd(wget_call)
+    
     filename = fn[8:]
     data_date = filename[-22:-14]
     f = h5py.File(filename, "r")
 
     group = f['Soil_Moisture_Retrieval_Data_AM']
     data_am = group['soil_moisture'][()]
-    print(process_data(data_am, data_date, 0))
+    print('events api ', process_data(data_am, data_date, 0))
 
     group = f['Soil_Moisture_Retrieval_Data_PM']
     data_pm = group['soil_moisture_pm'][()]
-    print(process_data(data_pm, data_date, 1))
+    print('events api ', process_data(data_pm, data_date, 1))
 
     f.close() 
 
 if __name__ == '__main__':
     main()
+    
